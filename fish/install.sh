4c186b3e73f326fcb775482294d398f2dbb4b531
amartin <alejandro.martin@henneo.com>
2020-02-17 12:27:24 +0100

    Fish configuration

---
+++ fish/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install fish
+
+chsh -s /usr/bin/fish
+omf install robyrussell
+omf theme robyrussell
---
+++ fish/oh-my-fish.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl -L https://get.oh-my.fish > install
+fish install --path=~/.local/share/omf --config=~/.config/omf
