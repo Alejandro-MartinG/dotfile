623abd9c9a3cd61bf7f47aac4bf3976e23cd6e63
amartin <alejandro.martin@henneo.com>
2020-03-23 13:22:30 +0100

    Fix incorrect name file

--- tools/vscode.sh
+++ tools/vscode.sh
@@ -2,4 +2,4 @@
 
 wget https://vscode-update.azurewebsites.net/latest/linux-deb-x64/stable -O /tmp/vscode_latest_amd64.deb
 sudo dpkg -i /tmp/vscode_latest_amd64.deb
-rm /tmp/vscode.deb
+rm /tmp/vscode_latest_amd64.deb
